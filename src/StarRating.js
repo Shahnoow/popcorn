a75182b7d983a649378850256f05747caaaa4040
shahnoow <shahnawazjawed9@gmail.com>
2025-04-18 00:16:11 +0500

    Initial commit

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { PropTypes } from "prop-types";
 const containStyle = {
   display: "flex",
   alignItems: "center",
@@ -19,18 +19,31 @@ const textStyle = {
   color: "yellow",
 };
 
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
+
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   size = 48,
   className = "",
   messages = [],
+  defaultRating = 0,
+  onSetRating,
 }) {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
   const textStyle = {
