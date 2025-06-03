1995139b1a317c75712e9690e0cc54746e22a549
shahnoow <shahnawazjawed9@gmail.com>
2025-06-04 00:14:23 +0500

    Initial commit

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { PropTypes } from "prop-types";
-const containStyle = {
+import PropTypes from "prop-types";
+
+const containerStyle = {
   display: "flex",
   alignItems: "center",
   gap: "16px",
@@ -8,15 +9,6 @@ const containStyle = {
 
 const starContainerStyle = {
   display: "flex",
-  gap: "4px",
-  BackgroundColor: "yellow",
-};
-
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-  BackgroundColor: "yellow",
-  color: "yellow",
 };
 
 StarRating.propTypes = {
@@ -52,8 +44,9 @@ export default function StarRating({
     color,
     fontSize: `${size / 1.5}px`,
   };
+
   return (
-    <div style={containStyle} className={className}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -83,6 +76,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     display: "block",
     cursor: "pointer",
   };
+
   return (
     <span
       role="button"
@@ -119,7 +113,10 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
   );
 }
 
-/*<svg
+/*
+FULL STAR
+
+<svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 20 20"
   fill="#000"
@@ -133,16 +130,6 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
 
 EMPTY STAR
 
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg> */
+
+
+*/
