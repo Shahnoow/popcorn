c747b1fe0cd429e9cf500c3ba75c5719cc1909ec
shahnoow <shahnawazjawed9@gmail.com>
2025-06-04 00:41:26 +0500

    Initial commit

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
-
-const containerStyle = {
+import { PropTypes } from "prop-types";
+const containStyle = {
   display: "flex",
   alignItems: "center",
   gap: "16px",
@@ -9,6 +8,15 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+  gap: "4px",
+  BackgroundColor: "yellow",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+  BackgroundColor: "yellow",
+  color: "yellow",
 };
 
 StarRating.propTypes = {
@@ -44,9 +52,8 @@ export default function StarRating({
     color,
     fontSize: `${size / 1.5}px`,
   };
-
   return (
-    <div style={containerStyle} className={className}>
+    <div style={containStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -76,7 +83,6 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     display: "block",
     cursor: "pointer",
   };
-
   return (
     <span
       role="button"
@@ -113,10 +119,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
   );
 }
 
-/*
-FULL STAR
-
-<svg
+/*<svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 20 20"
   fill="#000"
@@ -130,6 +133,16 @@ FULL STAR
 
 EMPTY STAR
 
-
-
-*/
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#000"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="{2}"
+    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+  />
+</svg> */
